eeabfbba860ad0b05c8d00ec296fec18e207e123
Diego <diegodatalab@gmail.com>
2023-08-26 15:16:11 -0300

    202. Scripts, Modules and the import Keyword

---
+++ boris/20_modules/202_scripts_modules_and_the_import_keyword/calculator.py
@@ -0,0 +1,15 @@
+creator = 'Diego'
+PI = 3.14159
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def subtract(a,b):
+    return a-b
+
+def area(radius):
+    return PI * radius * radius
+
