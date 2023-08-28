c9caacbacd96c451f5f70e52fc611f735a20a956
Diego <diegodatalab@gmail.com>
2023-08-28 20:29:48 -0300

    42. Parameters and Arguments

---
+++ boris/5_functions/42_parameters_and_arguments.py
@@ -0,0 +1,17 @@
+#
+# * 42. Parameters and Arguments
+
+
+def p(text):
+    print(text)
+
+p('teste')
+
+print()
+print('-'*50)
+
+def add(a,b):
+    print("the sum of", a, "and", b, "is", a + b)
+
+add(3,5)
+add(-4,7)
