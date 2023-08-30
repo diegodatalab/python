392741e72b022777df05d71e67f69143211f449b
Diego <diegodatalab@gmail.com>
2023-08-30 04:22:20 -0300

    44. Declaring Return Values for Functions

---
+++ boris/5_functions/44.py
@@ -0,0 +1,11 @@
+#
+# * 44. Declaring Return Values for Functions
+
+"""'return' word concludes the function. so nothing afterwards will ever run"""
+
+
+def add(a,b):
+    return a + b
+
+
+print(add(7,78))
